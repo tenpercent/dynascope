977f996f75476807a116c0079a28ade282b25562
max <maksim@cs.fsu.edu>
2020-03-22 23:26:13 -0400

    replace constraint layout with a frame one before implementing a settings menu

--- app/src/main/java/com/mobileproj/dynascope/FloatArrayUtil.kt
+++ app/src/main/java/com/mobileproj/dynascope/FloatArrayUtil.kt
@@ -40,8 +40,8 @@ fun FloatArray.innerproduct(other: FloatArray) = zip(other).map { it.first * it.
 
 fun FloatArray.autocorr(n: Int): Float {
     // drop first n measurements
-    val shifted = this.copyOfRange(n * 3, this.size)
-    val slice = this.sliceArray(0..shifted.size)
+    val shifted = copyOfRange(n * 3, this.size)
+    val slice = sliceArray(0..shifted.size)
     val innerProduct = slice.innerproduct(shifted)
     val shiftedNorm = sqrt(shifted.innerproduct(shifted))
     val sliceNorm = sqrt(slice.innerproduct(slice))
---
+++ app/src/main/java/com/mobileproj/dynascope/Preferences.kt
@@ -0,0 +1,10 @@
+package com.mobileproj.dynascope
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+}
