8fa4d5a40b93e05f8e38eaa0b400998748e848bd
max <maksim@cs.fsu.edu>
2020-03-23 18:02:46 -0400

    rewrite settings handler

--- app/src/main/java/com/mobileproj/dynascope/SettingsFragment.kt
+++ app/src/main/java/com/mobileproj/dynascope/SettingsFragment.kt
@@ -7,40 +7,41 @@ import androidx.preference.*
 class SettingsFragment(private val viewModel: SensorViewModel) : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
-        preferenceManager.apply {
-            findPreference<SeekBarPreference>("duration")?.apply {
-                onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, v ->
-                    viewModel.onDurationPreferenceChange(v)
+        preferenceManager.preferenceScreen.children.forEach { p ->
+            when (p.key) {
+                "duration" -> {
+                    p.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, v ->
+                        viewModel.onDurationPreferenceChange(v)
+                    }
+                    (p as SeekBarPreference).value = viewModel.sessionDurationValue
                 }
-                value = viewModel.sessionDurationValue
-            }
-            findPreference<SeekBarPreference>("speed")?.apply {
-                onPreferenceChangeListener =
-                    Preference.OnPreferenceChangeListener { _, v ->
+                "speed" -> {
+                    p.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, v ->
                         viewModel.onSpeedPreferenceChange(v)
                     }
-                value = viewModel.speedValue
-            }
-            findPreference<Preference>("reset")?.apply {
-                onPreferenceClickListener = Preference.OnPreferenceClickListener {
-                    viewModel.resetSessionProgress()
-                    true
+                    (p as SeekBarPreference).value = viewModel.speedValue
                 }
-            }
-            findPreference<SwitchPreferenceCompat>("voice")?.apply {
-                onPreferenceChangeListener =
-                    Preference.OnPreferenceChangeListener {_, v ->
+                "reset" -> {
+                    p.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+                        viewModel.resetSessionProgress()
+                        true
+                    }
+                }
+                "voice" -> {
+                    p.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, v ->
                         Log.d("debugsensor", "voice setting knob is $v")
                         true
                     }
-
-            }
-            findPreference<SwitchPreferenceCompat>("notifications")?.apply {
-                onPreferenceChangeListener =
-                    Preference.OnPreferenceChangeListener {_, v ->
+                }
+                "notifications" -> {
+                    p.onPreferenceChangeListener = Preference.OnPreferenceChangeListener {_, v ->
                         Log.d("debugsensor", "notification setting knob is $v")
                         true
                     }
+                }
+                else -> {
+                    Log.d("debugsensor", "no handler for $p")
+                }
             }
         }
     }
