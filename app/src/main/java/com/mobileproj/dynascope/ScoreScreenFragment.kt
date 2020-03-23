80ffc32843643f1ed923b8c39509d833f8083db9
max <maksim@cs.fsu.edu>
2020-03-23 09:20:22 -0400

    cleanup imports

--- app/src/main/java/com/mobileproj/dynascope/ScoreScreenFragment.kt
+++ app/src/main/java/com/mobileproj/dynascope/ScoreScreenFragment.kt
@@ -1,7 +1,6 @@
 package com.mobileproj.dynascope
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.INVISIBLE
@@ -11,11 +10,6 @@ import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import androidx.viewpager2.widget.ViewPager2
 import kotlin.math.max
 
 class ScoreScreenFragment(val viewmodel: SensorViewModel): Fragment() {
