cb18eae4f0719887668aae10069d3d5a599ecc11
max <maksim@cs.fsu.edu>
2020-03-23 20:41:07 -0400

    experiment with kotlin's sugar for operators and extensions

--- app/src/main/java/com/mobileproj/dynascope/Util.kt
+++ app/src/main/java/com/mobileproj/dynascope/Util.kt
@@ -3,15 +3,16 @@ package com.mobileproj.dynascope
 import java.util.ArrayDeque
 import kotlin.math.sqrt
 
-fun FloatArray.innerproduct(other: FloatArray) = zip(other).map { it.first * it.second }.sum()
+// a % b for inner product of 2 vectors
+operator fun FloatArray.rem(other: FloatArray) = zip(other).map { it.first * it.second }.sum()
 
 fun FloatArray.autocorr(n: Int): Float {
     // drop first n measurements
     val shifted = copyOfRange(n * 3, this.size)
     val slice = sliceArray(0..shifted.size)
-    val innerProduct = slice.innerproduct(shifted)
-    val shiftedNorm = sqrt(shifted.innerproduct(shifted))
-    val sliceNorm = sqrt(slice.innerproduct(slice))
+    val innerProduct = slice % shifted
+    val shiftedNorm = sqrt(shifted % shifted)
+    val sliceNorm = sqrt(slice % slice)
     return innerProduct / shiftedNorm / sliceNorm
 }
 
