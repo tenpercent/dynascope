f1362f25555b4df4773dc6bc7f81248b4704b95c
max <maksim@cs.fsu.edu>
2020-03-23 20:15:25 -0400

    cleanup imports

--- app/src/main/java/com/mobileproj/dynascope/SensorViewModel.kt
+++ app/src/main/java/com/mobileproj/dynascope/SensorViewModel.kt
@@ -4,9 +4,12 @@ import android.app.Application
 import android.content.Context
 import android.os.Vibrator
 import android.util.Log
-import androidx.lifecycle.*
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-import java.util.*
+import java.util.ArrayDeque
 
 // the responsibility is to hold displayed data independently of UI
 class SensorViewModel(application: Application): AndroidViewModel(application) {
