f384e9c5890db537d38abcbd04169f97f684fbf5
max <maksim@cs.fsu.edu>
2020-03-28 23:53:36 -0400

    more docs

--- app/src/main/java/com/mobileproj/dynascope/MainFragmentActivity.kt
+++ app/src/main/java/com/mobileproj/dynascope/MainFragmentActivity.kt
@@ -15,21 +15,37 @@ import androidx.viewpager2.widget.ViewPager2
 /**
  * The main and the only activity in this application.
  * It subclasses from [FragmentActivity] as we are using [ViewPager2]
- * and the adapter that provides fragments to viewpager to display
+ * and the [FragmentStateAdapter] that provides [Fragment]s to the [ViewPager2] to display
  * needs a reference to [FragmentActivity] in its constructor.
  */
 class MainFragmentActivity(): FragmentActivity() {
 
+    /**
+     * Retrieves [Sensor]s, attaches and detaches [SensorEventListener]s
+     */
     private val sensorManager: SensorManager by lazy {
         getSystemService(Context.SENSOR_SERVICE) as SensorManager
     }
+
+    /**
+     * Gyroscope [Sensor] produces a stream of (x, y, z) triples
+     * Values are larger with faster acceleration
+     */
     private val gyroSensor: Sensor by lazy {
         sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
     }
+
+    /**
+     * Holds the data that outlives UI
+     */
     private val sensorViewModel: SensorViewModel by lazy {
         ViewModelProvider(this).get(SensorViewModel::class.java)
     }
 
+    /**
+     * Sends sensor values to [sensorViewModel] on each sensor update.
+     * [sensorViewModel] does some computations that UI will pull to display
+     */
     private val gyroListener = object: SensorEventListener {
         override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
 
@@ -41,6 +57,10 @@ class MainFragmentActivity(): FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /**
+         * Tie [Fragment]s logic to [ViewPager2] UI container
+         * with [FragmentStateAdapter] converting [Int] page number into a [Fragment]
+         */
         findViewById<ViewPager2>(R.id.pager).adapter = object: FragmentStateAdapter(this) {
             override fun getItemCount() = 2
 
