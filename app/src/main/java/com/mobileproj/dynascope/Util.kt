090006bdf3c83e3ce8a761439790f14e4434b8b0
max <maksim@cs.fsu.edu>
2020-03-29 00:21:19 -0400

    docs for util

--- app/src/main/java/com/mobileproj/dynascope/Util.kt
+++ app/src/main/java/com/mobileproj/dynascope/Util.kt
@@ -3,9 +3,14 @@ package com.mobileproj.dynascope
 import java.util.ArrayDeque
 import kotlin.math.sqrt
 
-// a % b for inner product of 2 vectors
+/**
+ * [this] % [other] is an inner product of 2 vectors
+ */
 operator fun FloatArray.rem(other: FloatArray) = zip(other).map { it.first * it.second }.sum()
 
+/**
+ * Cosine similarity with shifted [this]
+ */
 fun FloatArray.autocorr(n: Int): Float {
     // drop first n measurements
     val shifted = copyOfRange(n * 3, this.size)
@@ -16,6 +21,9 @@ fun FloatArray.autocorr(n: Int): Float {
     return innerProduct / shiftedNorm / sliceNorm
 }
 
+/**
+ * Add elements to end while dropping from the front to ensure [this.size] == [capacity]
+ */
 fun<T> ArrayDeque<T>.addAndTrim(values: Sequence<T>, capacity: Int) {
     synchronized(this) {
         addAll(values)
